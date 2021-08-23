1177be439e24bbf83add6ab6e88dad2fd29a523b
Dilan <dilanh12@gmail.com>
2021-08-23 20:02:01 +1000

    Add files via upload
    Init commit

---
+++ main.js
@@ -0,0 +1,20 @@
+// Require
+const { Client, Intents } = require('discord.js');
+
+const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const { token }  = require('./config.json');
+
+const PREFIX = "-";
+
+bot.once('ready', () => {
+    console.log("Town Crier is online!");
+});
+
+bot.on("message", message => {
+    if (message.content == "hello") {
+        message.reply("Hear ye!");
+    }
+});
+
+
+bot.login(token);
